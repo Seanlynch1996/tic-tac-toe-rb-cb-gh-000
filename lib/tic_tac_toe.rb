44b251a3119a34697b53c7be89dedfa7bad49815
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2017-11-25 23:17:48 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -92,14 +92,18 @@ def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
-
-
-# Define your play method below
-def play(board)
-  9.times do
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
     turn(board)
   end
 end
+
 def turn_count(board)
   counter = 0
   board.each do |el|
